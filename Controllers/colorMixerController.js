ed0d346b2723eccd777b9ba7772a4997fed27099
kimvarde <kimaya.varde@gmail.com>
2017-09-02 21:25:56 -0400

    canvas right click implemented

--- Controllers/colorMixerController.js
+++ Controllers/colorMixerController.js
@@ -103,6 +103,24 @@ app.controller('ColorMixerController', ['$scope','$timeout',
         $scope.clearDrawing = function () {
             $scope.ctx.clearRect(0, 0, $scope.canvasWidth, $scope.canvasHeight);
         }
+
+        $scope.canvasRightclick = function (event) {
+
+            $scope.ctx.fillStyle = $scope.getRgbColor(); 
+            $scope.ctx.fillRect(0, 0, $scope.canvasWidth, $scope.canvasHeight);
+        }
         
 
     }]);
+
+app.directive('ngRightClick', function ($parse) {
+    return function (scope, element, attrs) {
+        var fn = $parse(attrs.ngRightClick);
+        element.bind('contextmenu', function (event) {
+            scope.$apply(function () {
+                event.preventDefault();
+                fn(scope, { $event: event });
+            });
+        });
+    };
+});
